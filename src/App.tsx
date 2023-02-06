83f84f9f48e2aee583d4aa166e0338ab04c11dfc
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-02-06 10:30:10 +0600

    router initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Routes, Route } from "react-router-dom";
+import { Container } from 'react-bootstrap';
+import Home from './pages/Home';
+import Store from './pages/Store';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/store' element={<Store />}></Route>
+          <Route path='/about' element={<About />}></Route>
+        </Routes>
+      </Container>
+    </>
   )
 }
 
---
+++ src/readme.md
@@ -0,0 +1,1 @@
+this is a simple cart project. made by typeScript and React.
