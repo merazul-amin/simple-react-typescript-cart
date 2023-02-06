e2d5e2636b1a75c0eacf8345e9c983f580861d75
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-02-06 10:12:49 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
